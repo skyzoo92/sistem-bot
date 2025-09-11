3166a64fd6dafb22fe392e06c920099723ff20a5
Hydra <145371709+skyzoo92@users.noreply.github.com>
2025-09-11 13:54:33 +0700

    Update baileys.js

--- system/baileys.js
+++ system/baileys.js
@@ -18,6 +18,17 @@ class BaileysBot extends EventEmitter {
         this.logger.level = "fatal";
     }
 
+    question(query) {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+        return new Promise(resolve => rl.question(query, ans => {
+            rl.close();
+            resolve(ans);
+        }));
+    }
+
     async system() {
         const { state, saveCreds } = await this.config.baileys.useMultiFileAuthState(
             this.config.sessions ? this.config.sessions : "Rapthalia-sessions"
