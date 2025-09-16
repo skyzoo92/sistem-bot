64a968126cfd28298cbcf44c056c341909b88e0f
Hydra <145371709+skyzoo92@users.noreply.github.com>
2025-09-17 00:07:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ Baileys: require("./system/baileys"),
 Sticker: require("./system/sticker"),
 Serialize: require("./system/simple"),
 Ezgif: require("./system/webp2mp4"),
+Button: require("./library/Button"),
 system: {
  converter: require("./system/converter"),
   exif: require("./system/exif"),
